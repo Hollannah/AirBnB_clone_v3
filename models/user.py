0919a9768a0ef7d2cdaf15570a9e8f4189e02cde
hollannah <olaideanna@gmail.com>
2023-08-05 13:43:46 -0700

    0x05. AirBnB clone - RESTful API

---
+++ models/user.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" holds class User"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
+
+class User(BaseModel, Base):
+    """Representation of a user """
+    if models.storage_t == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user", cascade="delete")
+        reviews = relationship("Review", backref="user", cascade="delete")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes user"""
+        from hashlib import md5
+        pas = kwargs.get("password", None)
+        if pas is not None:
+            kwargs.update({"password": md5(bytes(pas, "utf-8")).hexdigest()})
+        super().__init__(*args, **kwargs)
